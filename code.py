95169485e818fc293aba658a43dc8e43116c4a73
DaGamer12345 <DaGamer12345@users.noreply.github.com>
2016-03-08 17:23:38 -0600

    Added file support for script
    Takes input at message.txt and spits out any method of input to messagerino,txt

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
+import os.path # for file manip
 lastCharChecks = [".", ",", "?", '"', "s"] # exceptions to normal rule
 nonPlurals = ["this", "is"] # words to not try to make plural
 messagerino = [] # initialize table for finished words
-message = str(raw_input("Pleaserino entererino yourerino messagerino: \n")) # gets words from user
+messagerinoFile = open('messagerinoFile.txt', 'w') # opens file for writing
+if os.path.isfile('message.txt') == True: # if a message input file exists, use that instead of user input
+	messageFile = open('message.txt', 'r')
+	message = messageFile.read()
+	print("Usingerino messagerino filerino.")
+else: # if a message input file DOESN'T exist, use user input
+	message = str(raw_input("Pleaserino entererino yourerino messagerino: \n"))
 message = message.split() # splits words into table
 def messagerinoString(msg): # adds 'erino' or 'rino' depending on if the word ends in 'e'
 	if lastChar == "e":
@@ -25,5 +32,9 @@ for i in range(len(message)): # goes through word table and works on them
 		messagerino.append(str((messagerinoString(activeWord)) + lastChar)) # adds the last char to the end of the finished word
 	else:
 		messagerino.append(messagerinoString(activeWord)) # just goes through the normal process without dealing with words
-for i in range(len(messagerino)): # goes through the finished words table
+for i in range(len(messagerino)): # goes through the finished words table, writes to file
 	print(messagerino[i]), # prints all the words on 1 line
+	messagerinoFile.write(messagerino[i] + " ")
+messagerinoFile.close() # closes file used for writing
+if os.path.isfile('message.txt') == True: # closes input file if exists
+	messageFile.close()
